783e2315f4519678cd985b218828986a8b3094eb
Kenneth K. Slachta, Jr <ken.slachta@gmail.com>
2020-02-27 14:04:41 -0700

    initial commit

---
+++ apps/dashboard/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'phones-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dashboard';
+}
